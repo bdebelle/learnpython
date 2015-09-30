09a7582f1463c3c2827c3843ee129006bb98bab8
bdebelle <brian.debelle@gmail.com>
2015-09-29 20:33:47 -0700

    Tuesday Night Progress

---
+++ ex34.py
@@ -0,0 +1,9 @@
+letters = ['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+for i in letters:
+	print i
+
+print letters
+
+for i in range(len(letters)):
+	print "Index %d in the list is %s" % (i, letters[i])
