e34e63650dffe5558edd6aae30aec95f5e527b1d
bdebelle <brian.debelle@gmail.com>
2015-10-01 20:19:34 -0700

    lesson 35

--- ex35.py
+++ ex35.py
@@ -1,17 +1,18 @@
 from sys import exit
+# What is exit?
 
 def gold_room():
 	print "This room is full of gold. How much do you take?"
 
-	next = raw_input("> ")
-	if "0" in next or 1 in next:
+	next = int(raw_input("> "))
+	if next in range(0,49):
 		how_much = int(next)
 	else:
 		dead("Man, Learn to type a number.")
 
 	if how_much < 50:
 		print "Nice, you're not greedy. You win!"
-		exit(0)
+		exit(0) # this is an example of a good exit, No error will occur
 	else:
 		dead("You greedy bastard!")
 
@@ -38,6 +39,24 @@ def bear_room():
 		else:
 			print "I got no idea what that means."
 
+def matrix():
+	print "You are met by a man who intorduced himself as 'Morpheus'..."
+	print """ 
+This is your last chance.
+After this, there is no turning back.\n
+You take the blue pill
+-- the story ends, you wake up in your bed and believe whatever you want to believe.\n
+You take the red pill
+-- you stay in Wonderland and I show you how deep the rabbit hole goes.\n
+		"""
+	next = raw_input("blue or red?> ")
+
+	if "blue" in next:
+		print "You wake up in your bed, feeling as if youve had the strangest dream."
+	if "red" in next:
+		print "Welcome to the Nebuchadnezzar, Neo."
+	else:
+		bear_room()
 
 def cthulu_room():
 	print "Here you see the great evil Cthulu."
@@ -53,22 +72,25 @@ def cthulu_room():
 	else:
 		cthulu_room()
 
-
+#the function dead is called in numerous spots, it will print the contents
+# and explain why the user died. and then make a good exit to finish.
 def dead(why):
 	print why, "Good job!"
 	exit(0)
 
 def start():
 	print "You are in a dark room."
-	print "There is a door to your right and left"
+	print "There are three doors to choose from"
 	print "Which do you take?"
 
 	next = raw_input("> ")
 
-	if next == "left":
+	if next == "1":
 		bear_room()
-	elif next == "right":
+	elif next == "2":
 		cthulu_room()
+	elif next == "3":
+		matrix()
 	else:
 		dead("You stumble around the room until you starve.")
 
@@ -76,7 +98,14 @@ def start():
 
 
 
-start()
+start() # calls function 'start' to begin the script
+
+
+
+
+
+
+
 
 
 
