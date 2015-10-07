018b360de8c0f70978af3290812babd4c0f73bc1
bdebelle <brian.debelle@gmail.com>
2015-10-06 20:20:00 -0700

    added to lesson 35 sd

--- ex35.py
+++ ex35.py
@@ -1,4 +1,5 @@
 from sys import exit
+from time import sleep
 # Allows program to abort and the number inside exit() will indicate and error or not
 # 0 is a good exit, exit(100) will give different errors than exit(1) or 
 # exit(2)
@@ -20,40 +21,53 @@ def bear_room():
 	print "The bear has a bnunch of honey."
 	print "The fat bear is in front of another door"
 	print "How are you going to move the bear?"
+	print "Would you like to:"
+	print "A. Taunt Bear"
+	print "B. Take Honey"
 	bear_moved = False
 
 	while True:
 		next = raw_input("> ")
 
-		if next == "take honey":
+		if next == "take honey" or next == "b":
 			dead("The bear looks at you and slaps your face off.")
-		elif next == "taunt bear" and not bear_moved:
+		elif next == "taunt bear" or next == "a" and not bear_moved:
 			print "The bear has moved from the door. You can go through it now"
+			print "What do you want to do now?"
+			print "A. Taunt Bear"
+			print "B. Take Honey"
+			print "C. Open Door"
 			bear_moved = True
-		elif next == "taunt bear" and bear_moved:
+		elif next == "taunt bear" or next == "a" and bear_moved:
 			dead("The bear gets pissed and chews off your leg.")
-		elif next == "open door" and bear_moved:
+		elif next == "open door" or next == "c" and bear_moved:
 			gold_room()
 		else:
 			print "I got no idea what that means."
 
 def matrix():
 	print "You are met by a man who intorduced himself as 'Morpheus'..."
-	print """ 
-This is your last chance.
-After this, there is no turning back.\n
-You take the blue pill
--- the story ends, you wake up in your bed and believe whatever you want to believe.\n
-You take the red pill
--- you stay in Wonderland and I show you how deep the rabbit hole goes.\n
-		"""
+	sleep(2)
+	print "This is your last chance..."
+	sleep(2)
+	print "After this, there is no turning back..."
+	sleep(2)
+	print "You take the blue pill..."
+	sleep(3)
+ 	print "-- the story ends, you wake up in your bed \nand believe whatever you want to believe.\n"
+	sleep(4)
+	print "You take the red pill..."
+	sleep(3)
+	print "-- you stay in Wonderland and I show you \nrehow deep the rabbit hole goes.\n"
+	sleep(4)
 	next = raw_input("blue or red?> ")
 
 	if "blue" in next:
-		print "You wake up in your bed, feeling as if youve had the strangest dream."
-	if "red" in next:
-		print "Welcome to the Nebuchadnezzar, Neo."
+		dead("You wake up in your bed, feeling as if youve had the strangest dream.")
+	elif "red" in next:
+		dead("Welcome to the Nebuchadnezzar, Neo.")
 	else:
+		print "Wrong answer..."
 		bear_room()
 
 def cthulu_room():
@@ -74,12 +88,23 @@ def cthulu_room():
 # and explain why the user died. and then make a good exit to finish.
 def dead(why):
 	print why, "Good job!"
-	exit(0)
+	print "Want to play again?"
+	print "Y or N"
+
+	cont = raw_input(">")
+	if cont == "y":
+		print "Very Well..Good luck"
+		sleep(3)
+		start()
+	else:
+		print "GAME OVER"
+		exit(0)
 
 def start():
 	print "You are in a dark room."
 	print "There are three doors to choose from"
 	print "Which do you take?"
+	print "1, 2, or 3"
 
 	next = raw_input("> ")
 
@@ -97,10 +122,6 @@ def start():
 
 
 start() # calls function 'start' to begin the script
-
-
-
-
 
 
 
