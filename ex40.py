b4da142d5152b711d34a2efe372ec6608f73b72b
bdebelle <brian.debelle@gmail.com>
2015-10-29 15:40:16 -0700

    Ex fowdee

--- ex40.py
+++ ex40.py
@@ -36,3 +36,67 @@ thunder = ["City's looking like a ghost town",
 
 Song(thunder).sing_me_a_song()
 #Pass variable thunder to Class Song and perform function sing_me_a_song
+
+#Study Drill 3
+
+class Track(object):
+
+	def __init__(self, disk):
+		self.disk = disk
+		self.index = 0
+		self.jump()
+
+	def next(self):
+		'''jump to next item'''
+		self.index = (self.index + 1) % len(self.disk)
+		self.jump()
+
+	def prev(self):
+		'''jump to previous itme'''
+		self.index = (self.index - 1) % len(self.disk)
+		self.jump
+
+	def jump(self):
+		'''go to item'''
+		self.lyrics = self.disk[self.index]
+
+	def sing_song(self):
+		for line in self.lyrics:
+			print line
+
+
+track1 = ["Spring Time"]
+track2 = ["Summertime"]
+track3 = ["its Fall"]
+track4 = ["Winter is coming"]
+
+disk = [track1, track2, track3, track4]
+
+mycd = Track(disk)
+
+mycd.sing_song()
+
+mycd.next()
+mycd.sing_song()
+
+mycd.next()
+mycd.sing_song()
+
+mycd.next()
+mycd.sing_song()
+
+mycd.prev()
+mycd.sing_song()
+
+mycd.prev()
+mycd.sing_song()
+
+mycd.prev()
+mycd.sing_song()
+
+mycd.prev()
+mycd.sing_song()
+
+
+	
+
