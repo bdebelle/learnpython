e8d560cec67d95f4838424b8895dc80a36cb76f9
bdebelle <brian.debelle@gmail.com>
2015-11-30 20:31:46 -0800

    added cheat codes

--- ex43.py
+++ ex43.py
@@ -118,7 +118,11 @@ class LaserWeaponArmory(Scene):
 		print "wrong 10 times the the lock closes forever and you can't"
 		print "get the bomb. The code is 3 digits. Dont fuck this up."
 		code = "%d%d%d" % (randint(1,1), randint(1,1), randint(1,1))
+		
+		print code
+
 		guess = raw_input("[keypad]> ")
+		
 		guesses = 1
 
 		while guess != code and guesses < 10:
@@ -180,7 +184,10 @@ class EscapePod(Scene):
 		print "but you don't have time to look.  There's 5 pods, which one"
 		print "do you take?"
 
-		good_pod = randint(1,1)
+		good_pod = randint(1,5)
+
+		print good_pod
+
 		guess = raw_input("[pod #]> ")
 
 		if int(guess) != good_pod:
@@ -238,3 +245,54 @@ class Map(object):
 a_map = Map('central_corridor')
 a_game = Engine(a_map)
 a_game.play()
+
+
+
+
+"""
+class Scene(object):
+
+	def enter(self):
+		print "Enter is not yet configured"
+			
+		exit(1)
+
+class Engine(object):
+
+	def __init__(self, scene_map):
+		self.scene_map = scene_map
+
+	def play(self):
+		current_scene = self.scene_map.opening_scene()
+
+		while True:
+			print "\n---------"
+			next_scene_name = current_scene.enter()
+			current_scene = self.scene_map.next_scene(next_scene_name)
+
+class Map(object):
+
+	scenes = {
+		'central_corridor' : CentralCorridor(),
+		'laser_weapon_armory' : LaserWeaponArmory(),
+		'the_bridge' : TheBridge(),
+		'escape_pod' : EscapePod(),
+		'death' : Death(),
+		'finished' : Finished()
+	}
+
+	def __init__(self, start_scene):
+		self.start_scene = start_scene
+
+	def next_scene(self, scene_name):
+		return Map.scenes.get(scene_name)
+
+	def opening_scene(self):
+		return self.next_scene(self.start_scene)
+"""
+
+
+
+
+
+
