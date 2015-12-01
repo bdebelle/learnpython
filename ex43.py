9feed6c31e9294f3c3d5286dc7d27b0c420b42a0
bdebelle <brian.debelle@gmail.com>
2015-11-30 21:02:09 -0800

    Commit

--- ex43.py
+++ ex43.py
@@ -18,8 +18,7 @@ from random import randint
 class Scene(object):
 
 	def enter(self):
-		print "Enter is not yet configured"
-			
+		print "Enter is not yet configured"	
 		exit(1)
 
 class Engine(object):
@@ -248,48 +247,6 @@ a_game.play()
 
 
 
-
-"""
-class Scene(object):
-
-	def enter(self):
-		print "Enter is not yet configured"
-			
-		exit(1)
-
-class Engine(object):
-
-	def __init__(self, scene_map):
-		self.scene_map = scene_map
-
-	def play(self):
-		current_scene = self.scene_map.opening_scene()
-
-		while True:
-			print "\n---------"
-			next_scene_name = current_scene.enter()
-			current_scene = self.scene_map.next_scene(next_scene_name)
-
-class Map(object):
-
-	scenes = {
-		'central_corridor' : CentralCorridor(),
-		'laser_weapon_armory' : LaserWeaponArmory(),
-		'the_bridge' : TheBridge(),
-		'escape_pod' : EscapePod(),
-		'death' : Death(),
-		'finished' : Finished()
-	}
-
-	def __init__(self, start_scene):
-		self.start_scene = start_scene
-
-	def next_scene(self, scene_name):
-		return Map.scenes.get(scene_name)
-
-	def opening_scene(self):
-		return self.next_scene(self.start_scene)
-"""
 
 
 
