fc079cdbca0678c2e1ed399d8fa517b95e2473e9
bdebelle <brian.debelle@gmail.com>
2016-03-31 18:31:41 -0700

    Back at again

--- projects/gesso/build/lib/test2/printstring.py
+++
@@ -1,2 +0,0 @@
-def printstring(string="you didn't provide a string"):
-    print(string)
--- projects/gesso/build/scripts-2.7/testscript.py
+++
@@ -1,4 +0,0 @@
-from test2 import printstring
-
-printstring.printstring("this is a test")
-print("test over")
